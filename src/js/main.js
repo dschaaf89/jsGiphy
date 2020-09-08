8d4918932543091978f65c5724d7dc04097914f5
Mike Manchee <mikemanchee@gmail.com>
2020-09-08 14:56:11 -0700

    add functions to trending and random images
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- src/js/main.js
+++ src/js/main.js
@@ -3,36 +3,39 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
 
+
 $(document).ready(function () {
-  $("#search").click(function (event) {
+  $("#search").click(function(event) {
     event.preventDefault();
     const search = $("#image").val();
+    $("#image").val("");
 
-    const url = `api.giphy.com/v1/gifs/search?api_key${apikey}&q=${search}`;
-    
     let request = new XMLHttpRequest();
-      request.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          i = 0 ? getSearch(response) : i = 1 ? getTrending(response) : getRandom(response);
-          ;
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
 
-      function getSearch(response) {
-        let image;
-        for (let i = 0; i < 16; i++) {
-          let image = `<img src=${response.data[i].images.downsized_small.url}>`
-          $("#image" + (i + 1)).html(image);
-        }
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${search}`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getSearch(response);
       }
+    };
+    request.open("GET", url, true);
+    request.send();
+
+    function getSearch(response) {
+      let image;
+      
+      for (let i = 0; i < 16; i++) {
+        image = `<img src=${response.data[i].images.downsized_medium.url}>`;
+        $("#image" + (i + 1)).html(image);
+      }
+    }
   });
-  $("#trending").click(function (event) {
+  $("#trending").click(function(event) {
     event.preventDefault();
 
-    const url = `api.giphy.com/v1/gifs/trending?api_key${apikey}`;
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}`;
 
     let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -43,15 +46,19 @@ $(document).ready(function () {
     };
     request.open("GET", url, true);
     request.send();
-    
-    function getTrending(response) {
 
+    function getTrending(response) {
+      let image;
+      for (let i = 0; i < 16; i++) {
+        image = `<img src=${response.data[i].images.downsized_medium.url}>`;
+        $("#image" + (i + 1)).html(image);
+      }
     }
   });
-  $("#random").click(function (event) {
+  $("#random").click(function(event) {
     event.preventDefault();
 
-    const url3 = `api.giphy.com/v1/gifs/random?api_key${apikey}`;
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}`;
 
     let request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -64,9 +71,10 @@ $(document).ready(function () {
     request.send();
 
     function getRandom(response) {
-
+      let image = `<img src=${response.data.images.downsized_medium.url}>`;
+      $("#image1").html(image);
     }
-  });  
+  });
 });
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'jsGiphy',
+      title: 'jsgiphy',
       template: './src/index.html',
       inject: 'body'
     })
